30342b8966e54c8d72ed42263ce317ff17752d8c
Maksym Bobryshev <maksym.bobmm@gmail.com>
2019-07-04 15:23:45 +0300

    score calculation stop works. Need to look previous version

--- logic.rb
+++ logic.rb
@@ -15,7 +15,15 @@ class MyReader
     file = File.open(ARGV[0])
     file.each do |line|
       club_row = line.split(',')
-      club_row.each { |club| @clubs[club.strip[0..-3].to_sym] = 0 }
+      puts "club row |#{club_row}|"
+      # TODO improve finding club name (now supposed to score cant be > 9) strip[0..-3].to_sym
+      # club_row.each { |club| @clubs[club.strip[0..-3].to_sym] = 0 }
+      club_row.map! do |club|
+        var = club.strip
+        var = var.reverse.sub(' ', '|').reverse
+        var.split('|')
+      end
+      puts "club row final |#{club_row}|\n\n"
     end
     file.close
   end
@@ -43,10 +51,13 @@ class MyReader
       @clubs[arr[1][0].to_sym] += 3
     else
       # puts " qweqw #{arr[0][0]} #{@clubs[arr[0][0]]}"
+      puts "clubs === #{@clubs}"
+      puts "first #{arr[0][0]}"
+      puts "arr |#{@clubs[arr[0][0].to_sym]}|"
       @clubs[arr[0][0].to_sym] += 1
       @clubs[arr[1][0].to_sym] += 1
     end
-    puts "clubs #{@clubs}"
+    # puts "clubs #{@clubs}"
   end
 
   def prepare_line(line)
@@ -64,11 +75,16 @@ class MyReader
     arr
   end
 
+  def print
+    @clubs.sort_by { |k,v| v }.reverse!
+  end
+
   def initialize
     check_args
     fill_clubs_names
-    puts @clubs
     fill_clubs_score
+    print
+    # puts @clubs
   end
 end
 
